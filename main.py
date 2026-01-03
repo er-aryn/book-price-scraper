8dd41c8ee1210477f0ad2cb767b4c9260d1e0516
er-aryn <aryanraj79001@gmail.com>
2026-01-03 20:16:21 +0530

    add pagination

--- main.py
+++ main.py
@@ -2,29 +2,41 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-data=[]
-
+data = []
 url = "https://books.toscrape.com/"
-response= requests.get(url)
 
+while True:
 
-soup = BeautifulSoup(response.text,"html.parser")
-cards=soup.find_all("article", class_="product_pod")
+    print("Scraping:", url)
 
-for card in cards:
-    h3=card.find("h3")
-    a_tag = h3.find("a")
-    title = a_tag.get("title")
-    raw_price=card.find("p" , class_="price_color").text
-    price=raw_price.replace("£", "").replace("Â", "")
-    data.append({
-        "title":title,
-        "price":price
-    })
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    cards = soup.find_all("article", class_="product_pod")
+
+    for card in cards:
+        title = card.find("h3").find("a").get("title")
+        raw_price = card.find("p", class_="price_color").text
+        price = raw_price.replace("£","").replace("Â","")
+
+        data.append({
+            "title": title,
+            "price": price
+        })
+
+    nxt_btn = soup.find("li", class_="next")
+
+    if not nxt_btn:
+        break
+
+    next_link = nxt_btn.find("a")["href"]
+
+    if "catalogue" not in url:
+        url = "https://books.toscrape.com/" + next_link
+    else:
+        url = "https://books.toscrape.com/catalogue/" + next_link
+
+        print(len(data))
 df=pd.DataFrame(data)
-df.to_csv("books.csv", index=False)
-
-print("Saved!")
-
-
-
+df.to_csv("books_all_pages.csv", index=False)
+print("saved")
